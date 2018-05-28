7969b4bdd44323fa455efa94401a980c9579b4c6
Muhammad Abbas <mohammed.petiwala52@gmail.com>
2018-05-28 11:36:14 +0500

    sms test working

---
+++ app/src/main/java/com/example/user/myapplication/BroadcastActivity.java
@@ -0,0 +1,64 @@
+package com.example.user.myapplication;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class BroadcastActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_broadcast);
+    }
+
+    public void onClickBroadcastButtonListener(View view) {
+        Log.v("MAP", "send sms ?");
+
+        ActivityCompat.requestPermissions(BroadcastActivity.this,
+                new String[]{Manifest.permission.SEND_SMS},
+                1);
+    }
+
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case 1: {
+
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+                    Log.v("MAP", "permission granted");
+                    EditText edit = (EditText)findViewById(R.id.editText2);
+                    EditText edit1 = (EditText)findViewById(R.id.editText);
+
+                    SmsManager smsManager = SmsManager.getDefault();
+                    smsManager.sendTextMessage(edit1.getText().toString(), null, edit.getText().toString(), null, null);
+
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                    Toast.makeText(BroadcastActivity.this, "Permission denied to read your External storage", Toast.LENGTH_SHORT).show();
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
+    }
+}
--- app/src/main/java/com/example/user/myapplication/MainActivity.java
+++ app/src/main/java/com/example/user/myapplication/MainActivity.java
@@ -1,15 +1,21 @@
 package com.example.user.myapplication;
 
 import android.annotation.SuppressLint;
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import static android.view.View.*;
+
 public class MainActivity extends AppCompatActivity {
     public static final String MY_TAG = "my_custom_message";
+
+    private static Button button_boadcast;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,14 +24,9 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public void onButtonClick(View v) {
-        EditText e1 = (EditText)findViewById(R.id.editText);
-        EditText e2 = (EditText)findViewById(R.id.editText2);
-        TextView t1 = (TextView)findViewById(R.id.textView2);
-
-        int num1 = Integer.parseInt(e1.getText().toString());
-        int num2 = Integer.parseInt(e2.getText().toString());
-
-        t1.setText(Integer.toString(num1+num2));
+    public void onClickButtonListener(View view) {
+        Log.v("MAP", "is this for real ?");
+        Intent intent = new Intent(this, BroadcastActivity.class);
+        startActivity(intent);
     }
 }
