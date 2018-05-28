b24f10518e7eda0ac1b2ae10524997417e14fec2
Muhammad Abbas <mohammed.petiwala52@gmail.com>
2018-05-29 00:19:59 +0500

    wip: read csv file into a list

--- app/src/main/java/com/example/user/myapplication/MainActivity.java
+++ app/src/main/java/com/example/user/myapplication/MainActivity.java
@@ -3,12 +3,18 @@ package com.example.user.myapplication;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import com.opencsv.CSVReader;
+
+import java.io.FileReader;
+import java.util.List;
 
 import static android.view.View.*;
 
@@ -24,9 +30,17 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public void onClickButtonListener(View view) {
+    public void onClickBroadcastButtonListener(View view) {
         Log.v("MAP", "is this for real ?");
         Intent intent = new Intent(this, BroadcastActivity.class);
         startActivity(intent);
+    }
+
+    public void onClickUpdateContactsButtonListener(View view) {
+        Log.v("MAP", "read file");
+        Log.v("MAP", Environment.getExternalStorageDirectory().getAbsolutePath());
+        CSVReader reader = new CSVReader(new FileReader(Environment.getExternalStorageDirectory().getAbsolutePath()+"/Download/filtered.csv"));
+        List myEntries = reader.readAll();
+
     }
 }
